b0d893203c02c4ba211a021c588f3c5ad5900074
matheusfdosan <matheusfaustinoe20@gmail.com>
2025-02-25 09:00:12 -0300

    :tada: initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import fastify from "fastify"
+const server = fastify({ logger: true })
+import { PORT } from "./envVariables.js"
+
+server.listen(
+  {
+    port: PORT,
+    host: "0.0.0.0",
+  },
+  (err) => {
+    if (err) throw err
+    console.log("Servidor rodando na porta 3000")
+  }
+)
