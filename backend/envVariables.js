7831429f6008259f83dc3a87b1859a41e865bb00
Matheus Faustino <104006185+matheusfdosan@users.noreply.github.com>
2025-02-25 16:53:47 -0300

    Delete backend directory

--- backend/envVariables.js
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv"
-
-config({ path: "./src/configs/.env" })
-
-export const PORT = process.env.PORT
-export const PGHOST = process.env.PGHOST
-export const PGDATABASE = process.env.PGDATABASE
-export const PGUSER = process.env.PGUSER
-export const PGPASSWORD = process.env.PGPASSWORD
--- backend/src/configs/database.js
+++
@@ -1,6 +0,0 @@
-import postgres from "postgres";
-import { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } from "../../envVariables.js"
-
-const URL = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
-
-export const sql = postgres(URL)
