735a1493a8d909bb089078c026b9dbc26cc926e9
matheusfdosan <matheusfaustinoe20@gmail.com>
2025-03-09 17:46:38 -0300

    feat: purchases items

--- frontend/src/components/Allpurchases/index.jsx
+++ frontend/src/components/Allpurchases/index.jsx
@@ -1,9 +1,20 @@
 import './style.css'
+import search_icon from '../../assets/icon-search.png'
+
+import Purchase from '../../components/Purchase'
 
 function Allpurchases () {
     return (
-        <div>
-           
+        <div className='Pcontainer'>
+           <h2>Todas as Compras</h2>
+           <input type="text" className='Search_input' placeholder='Carl Barang'/>
+           <span className='search_icon'><img src={search_icon} alt="" /></span>
+           <div className='Plist'>
+           <Purchase />
+           <Purchase />
+           <Purchase />
+           </div>
+
         </div>
     )
 }
