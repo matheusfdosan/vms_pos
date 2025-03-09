20c9bccc13aa2a3429dbdb4d73dd30382dd8551d
PedroCabs <166236768+PedroCabs@users.noreply.github.com>
2025-03-09 14:03:30 -0300

    Add files via upload

---
+++ frontend/NewArq/Purchase/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Purchase() {
+    return(
+        <>
+        <div className='Purchase'>
+            <span><strong>Pedro Cabral comprou R$ 267,68 em produtos</strong></span>
+            <button className='invoice_btn'>Ver nota Fiscal</button>
+        </div>
+        </>
+    )
+}
+
+export default Purchase
