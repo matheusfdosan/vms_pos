98fdde607dd044574add48022c83e7c0beb3daee
Alunos <alunos@pedreira-local.org>
2025-03-11 14:34:36 -0300

    refactor: no more random code

--- backend/src/services/newProductService.js
+++ backend/src/services/newProductService.js
@@ -1,8 +1,7 @@
 import { sql } from "../configs/database.js"
 
 export default async function newProductService(productsData) {
-  const id = Math.floor(10000 + Math.random() * 90000)
-  const { img, name, price, stock } = productsData
+  const { id, img, name, price, stock } = productsData
 
   try {
     await sql`INSERT INTO product(id, name, img, price, stock) VALUES (${id}, ${name}, ${img}, ${price}, ${stock})`
